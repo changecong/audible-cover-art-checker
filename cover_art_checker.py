adc3f46f901851ec0a47770f9dd9afcb4435df09
Zhicong Chen <zhicongc@audible.com>
2014-07-31 11:02:28 -0400

    modified

--- cover_art_checker.py
+++ cover_art_checker.py
@@ -4,8 +4,8 @@
 ## Version:       
 ## Author:        Zhicong Chen <zhicong.chen@changecong.com>
 ## Created at:    Wed Jul 30 20:31:45 2014
-## Modified at:   Wed Jul 30 23:00:03 2014
-## Modified by:   Zhicong Chen <zhicong.chen@changecong.com>
+## Modified at:   Thu Jul 31 11:00:00 2014
+## Modified by:   Zhicong Chen <zhicongc@audible.com>
 ## Status:        Experimental, do not distribute.
 ## Description:   This is a small tool that used to check if cover arts
 ##                of Audible audio books exist
@@ -15,7 +15,7 @@
 import sys
 import urllib2
 from bs4 import BeautifulSoup 
-
+import threading
 
 # a list of error code
 INVALID_ARGV = "invalid_argv"
@@ -54,7 +54,7 @@ class Utility:
 
 class BookUtil:
 
-    def book_file_name_checker(sys_argvs):
+    def book_file_name_checker(self, sys_argvs):
 
         if not sys_argvs:
             return INVALID_ARGV
@@ -66,8 +66,8 @@ class BookUtil:
 
         # check if the file exist
         import os
-        file_exits = os.path.isfile(file_name)
-        if file_exist:
+        file_exists = os.path.isfile(file_name)
+        if file_exists:
             return file_name
         else:
             return INVALID_FILE_NAME
@@ -90,54 +90,117 @@ class AudibleUtil:
         # page = urllib2.urlopen('http://www.leeon.me');
 
 
-    def check_cover_art():
+    def check_cover_art(self):
         
+        
+        len_asins = 30
+        total_asins = 0
+        thread_num = 0
+
+
+        all_asins = []
+
+
+        file = open(self.__filename);
+
+        while 1:
+            line = file.readline()
+            all_asins.append(line)
+            if not line:
+                break
         # create a new thread
         
+        total_asins = len(all_asins)
+        
+        thread_num = total_asins / len_asins
+        #print thread_num
+        #sys.exit(0)
+        print str(total_asins) + " asins in Total"
+        print str(len_asins) + " asins in each group"
+        
+        if total_asins % len_asins != 0:
+            thread_num = thread_num + 1
 
+        print str(thread_num) + " groups"
+        
+        for thread_id in range(thread_num):
+
+            if len(all_asins) < len_asins:
+                asins = all_asins
+            else:
+                asins = all_asins[0: len_asins]
+                all_asins = all_asins[len_asins : ]
+
+            thread = AudibleThread(thread_id, asins)
+            thread.start()
 
         
 class AudibleThread(threading.Thread):
 
-    def __init__(self, threadID, asin):
+    '''
+    threadID - the id of the current thread
+    asins    - a list of asins
+
+    '''
+    def __init__(self, threadID, asins):
         threading.Thread.__init__(self)
         self.__threadID = threadID
-        self.__asin = asin
+        self.__asins = asins
         self.__url_base = "http://www.audible.com/search/ref=?advsearchKeywords="
 
     def run(self):
         # run the task
-        check_cover_art()
+        print "Thread-" + str(self.__threadID) + " start."
+        self.check_cover_art()
+        print "Thread-" + str(self.__threadID) + " end."
+        # print self.__asins
+
+    def check_cover_art(self):
         
+        for asin in self.__asins:
 
-    def check_cover_art():
+            # assemble the url
+            url = self.__url_base + asin
+
+            # load the page
+            page = urllib2.urlopen(url)
+            soup = BeautifulSoup(page)
+
+            image_url = ''
+
+            try:
+                search_result = soup.find("ul", class_='adbl-search-results')
+                search_result_soup = BeautifulSoup(str(search_result))
+                image_url = search_result_soup.find("img", class_='adbl-prod-image')['src']
+                # print image_url
+            except:
+                print "Exception: " + asin + "may be an invalid ASIN"
+            
+            # <img alt="The Hobbit, Part 1 | J. R. R. Tolkien" class="adbl-prod-image" src="http://ecx.images-amazon.com/images/I/51Xek6+7QwL._SL150_.jpg"/>
         
-        # assemble the url
-        url = self.__url_base + self.__asin
+            # a fast way to verify is to check if there is "audilb" in the image src
+            # print asin + " : " + image
+            
 
-        # load the page
-        page = urllib2.urlopen(url)
-        soup = BeautifulSoup(page)
+            key_word = "Audible"
 
-        image = soup.find("img", class_='adbl-prod-image')['src']
-        
-        # <img alt="The Hobbit, Part 1 | J. R. R. Tolkien" class="adbl-prod-image" src="http://ecx.images-amazon.com/images/I/51Xek6+7QwL._SL150_.jpg"/>
-        
-        # a fast way to verify is to check if there is "audilb" in the image src
+            no_image = image_url.find(key_word)
 
-        key_word = "Aubible"
-
-        no_image = image.find(key_word)
-
-        if no_image != -1:
-            print self.__asin
+            # if the keyword is found
+            if no_image != -1:
+                print asin + " <--- Yes!!! Find it"
+                print image_url
 
 
 if __name__ == '__main__':
 
     # check the validity of the book list file then return the vilad file name 
-    booklist = BookUtil.book_file_name_checker(sys.argv);
-    app_util = AppUtil()
+    book_util = BookUtil()
+
+    booklist = book_util.book_file_name_checker(sys.argv);
+
+    print booklist
+    app_util = Utility()
 
     if booklist is INVALID_ARGV:
         app_util.display_help()
--- example.py
+++ example.py
@@ -4,8 +4,8 @@
 ## Version:       
 ## Author:        Zhicong Chen <zhicong.chen@changecong.com>
 ## Created at:    Wed Jul 30 22:06:54 2014
-## Modified at:   Wed Jul 30 22:33:22 2014
-## Modified by:   Zhicong Chen <zhicong.chen@changecong.com>
+## Modified at:   Thu Jul 31 10:39:42 2014
+## Modified by:   Zhicong Chen <zhicongc@audible.com>
 ## Status:        Experimental, do not distribute.
 ## Description:   
 ##
@@ -14,9 +14,23 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-url = "http://www.audible.com/search/ref=?advsearchKeywords=B0030EJV3U"
-# url = "http://www.audible.com/search/ref=?advsearchKeywords=B002UUKMM4"
+# url = "http://www.audible.com/search/ref=?advsearchKeywords=B0030EJV3U"
+url = "http://www.audible.com/search/ref=?advsearchKeywords=B002UUKMM4"
+# url = "http://www.audible.com/search/ref=?advsearchKeywords=B007IKJYT8"
 page = urllib2.urlopen(url)
 soup = BeautifulSoup(page)
 
-print soup.find("img", class_='adbl-prod-image')['src']
+#image = soup.find("img", class_='adbl-prod-image')['src']
+#print image
+
+search_result = soup.find("ul", class_='adbl-search-results')
+print search_result
+search_result_soup = BeautifulSoup(str(search_result))
+image = search_result_soup.find("img", class_='adbl-prod-image')['src']
+
+
+key_word = "Audible"
+no_image = image.find(key_word)
+# if the keyword is found                                                                                                                                                                            
+if no_image != -1:
+    print image
